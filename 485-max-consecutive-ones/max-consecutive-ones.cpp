6e916a95e9a1df2d59cdcf3ce8ccf67fb330a2a3
Дмитрий Чернокожев <146494017+ChernokozhevDmitry@users.noreply.github.com>
2024-02-28 13:31:43 +0300

    Time: 166 ms (5.06%) | Memory: 38.4 MB (47.13%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -6,17 +6,11 @@ public:
             std::cout << "i = "<< i << std::endl;
             if (i == 1) {
                 ++chain_cur;
-                std::cout << "chain_cur = "<< chain_cur << std::endl;
+                chain_max = max(chain_max, chain_cur);
             }
             else {
-                if (chain_cur > chain_max) {
-                    chain_max = chain_cur;
-                }
                 chain_cur = 0;
             }
-        }
-        if (chain_cur > chain_max) {
-            chain_max = chain_cur;
         }
         return chain_max;
     }
