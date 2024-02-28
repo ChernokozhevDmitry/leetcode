ca4417947f533c5a92b4b5901b31352e0af3721b
Дмитрий Чернокожев <146494017+ChernokozhevDmitry@users.noreply.github.com>
2024-02-28 13:31:58 +0300

    Time: 33 ms (21.52%) | Memory: 38.7 MB (22.24%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -3,7 +3,6 @@ public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int chain_max = 0, chain_cur = 0;
         for(auto i: nums){
-            std::cout << "i = "<< i << std::endl;
             if (i == 1) {
                 ++chain_cur;
                 chain_max = max(chain_max, chain_cur);
