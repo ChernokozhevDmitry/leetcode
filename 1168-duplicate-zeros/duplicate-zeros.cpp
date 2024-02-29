4f904e66d7b3dcda743da31e3d7a39b90ef26c38
Дмитрий Чернокожев <146494017+ChernokozhevDmitry@users.noreply.github.com>
2024-02-29 23:27:37 +0300

    Time: 7 ms (57.81%) | Memory: 11.9 MB (56.07%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.cpp
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        std::vector<int> newarr(arr.size(),0);
-        for (int i = 0, j = 0; i < newarr.size(); ++i, ++j) {
-            newarr[i] = arr[j];        
-            if (arr[j] == 0) {
+        for (int i = 0; i < arr.size(); ++i) {
+            if (arr[i] == 0) {
+                arr.resize(arr.size() - 1);
+                arr.insert(arr.begin() + i, 0);
                 ++i;
+                std::cout << "   ++i = " << i << std::endl;
             }
         }
-        arr = newarr;
     }
 };
