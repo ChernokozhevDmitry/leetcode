99a8e123597af596dcddc7e9831e54ee8a69e781
Дмитрий Чернокожев <146494017+ChernokozhevDmitry@users.noreply.github.com>
2024-03-04 09:31:07 +0300

    Time: 8 ms (34.10%) | Memory: 12.4 MB (46.49%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        for (int i = 0; i < arr.size(); ++i){
-            for (int j = i + 1; j < arr.size(); ++j){
+        int n = arr.size();
+        for (int i = 0; i < n; ++i){
+            for (int j = i + 1; j < n; ++j){
             if ((arr[i] == 2 * arr[j]) || (arr[j] == 2 * arr[i])) {
                      return true;
                 }
