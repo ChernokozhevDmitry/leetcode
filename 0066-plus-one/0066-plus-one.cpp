10d02d23b27791d34f932867b64bcc46737008e2
Дмитрий Чернокожев <146494017+ChernokozhevDmitry@users.noreply.github.com>
2024-04-10 16:22:11 +0300

    Time: 0 ms (100%), Space: 10.3 MB (15.35%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        bool next_nine = false;
-        if (digits.back() == 9){
-            next_nine = true;
-            digits.back() = 0;
-            for (auto i = digits.rbegin() + 1; i != digits.rend(); ++i){
-                if (next_nine){
-                    if (*i == 9){
-                        *i = 0;
-                        next_nine = true;
-                    }
-                    else {
-                        *i += 1;
-                        next_nine = false;
-                    }
-                }
+        for (auto i = digits.rbegin(); i != digits.rend(); ++i){
+            if (*i < 9){
+                (*i)++;
+                return digits;
             }
-            if (next_nine){
-                digits[0] = 1;
-                digits.push_back(0);
-            }
+            *i = 0;
         }
-        else {
-            digits.back() += 1;
-        }
+        digits[0] = 1;
+        digits.push_back(0);
         return digits;
     }
 };
