51865dfa5dbf27a2385a726ac8fc9446a97983db
Дмитрий Чернокожев <146494017+ChernokozhevDmitry@users.noreply.github.com>
2024-04-16 23:54:44 +0300

    Time: 27 ms (48.45%), Space: 22 MB (45.07%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -2,42 +2,38 @@ class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         std::vector<int> res;
-        int row = mat.size();
-        int col = mat[0].size();
-        int total = row*col;
-        int k=0, i=0, j=0;
-        bool up=true;
-        while(k<total) {
-            res.push_back(mat[i][j]);
-            if(up) {
-                if(i==0 && j<col-1) {
-                   j++;
-                   up=!up;
+        int row = 0, col = 0, rowsize = mat.size(), colsize = mat[0].size();
+        bool up = true;
+        for(int k = 0; k < colsize * rowsize; ++k){
+            res.push_back(mat[row][col]);
+            if (up){
+                if ((row == 0)&&(col < colsize - 1)){
+                    ++col;
                 }
-                else if(j== col-1) {
-                    i++;
-                    up= !up;
+                else if (col == colsize - 1){
+                    ++row;
                 }
                 else {
-                    i--;
-                    j++;
+                    --row;
+                    ++col;
+                    continue;
                 }
+                up = !up;
             }
             else {
-                if(i<row-1 && j==0) {
-                    i++;
-                    up=!up;
+                if ((row < rowsize - 1)&&(col == 0)){
+                    ++row;
                 }
-                else if(i == row -1) {
-                    j++;
-                    up = !up;
+                else if (row == rowsize - 1){
+                    ++col;
                 }
                 else {
-                    i++;
-                    j--;
+                    ++row;
+                    --col;
+                    continue;
                 }
+                up = !up;
             }
-            k++;
         }
         return res;
     }
