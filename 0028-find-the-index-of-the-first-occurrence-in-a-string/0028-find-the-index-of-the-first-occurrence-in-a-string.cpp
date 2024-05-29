bf5409b547a2054ab9560d8a51e0d264b40d1851
Дмитрий Чернокожев <146494017+ChernokozhevDmitry@users.noreply.github.com>
2024-05-30 00:21:40 +0300

    Time: 0 ms (100%), Space: 7.5 MB (49.84%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int res = haystack.find(needle);
+        if (res == std::string::npos) {
+            return -1;
+        }
+        return res;
+    }        
+};
