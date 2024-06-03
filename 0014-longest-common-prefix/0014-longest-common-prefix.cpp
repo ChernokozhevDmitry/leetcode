6ea05ce4acf66182fc32bcdea46117609901f329
Дмитрий Чернокожев <146494017+ChernokozhevDmitry@users.noreply.github.com>
2024-06-03 15:52:03 +0300

    Time: 4 ms (38.74%), Space: 10.7 MB (98.23%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        std::string ans="";
+        for(int j = 0; j != strs[0].size(); ++j){
+            for(int i = 1; i != strs.size(); ++i){
+                if (j > (strs[i].size() - 1)){
+                    return ans;
+                }
+                else if (strs[0][j] != strs[i][j]){
+                    return ans;
+                }
+            }
+            ans+=strs[0][j];
+            std::cout << "ans = " << ans << std::endl;
+        }
+        return ans;   
+    }
+};
