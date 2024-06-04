e6123a93dddf1556c71ca0131d7e873319029f63
Дмитрий Чернокожев <146494017+ChernokozhevDmitry@users.noreply.github.com>
2024-06-04 11:08:20 +0300

    Time: 40 ms (51.14%), Space: 31.7 MB (73.3%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -3,8 +3,8 @@ public:
     int arrayPairSum(vector<int>& nums) {
         int res {0};
         std::sort(nums.begin(), nums.end());
-        for(std::vector<int>::iterator i = nums.begin(); i != nums.end(); i += 2){
-            res += std::min(*i, *(i + 1));
+        for(int i = 0; i != nums.size(); i += 2){
+            res += nums[i];
         }
         return res;        
     }
