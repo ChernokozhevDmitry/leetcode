7246db7fa38f67442d09b6f886260b527f2d7082
Дмитрий Чернокожев <146494017+ChernokozhevDmitry@users.noreply.github.com>
2024-06-04 16:03:05 +0300

    Update 0167-two-sum-ii-input-array-is-sorted.cpp
    not right (too many time)

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         std::vector<int> res;
-        std::vector<int>::iterator it = numbers.begin(), jt = numbers.end() - 1;
-        while(it < jt){
-            if (target == (*it + *jt)){
+        for(std::vector<int>::iterator it = numbers.begin(); it != numbers.end(); ++it){
+            for(std::vector<int>::iterator jt = it + 1; jt != numbers.end(); ++jt){
+                if (target == (*it + *jt)){
                     res.push_back(std::distance( numbers.begin(), it ) + 1);
                     res.push_back(std::distance( numbers.begin(), jt ) + 1);
                     return res;
+                }
             }
-            else if (target > (*it + *jt)) {
-                ++it;
-            } else {
-                --jt;
-            }
-        } 
+        }        
         return res;
     }
 };
